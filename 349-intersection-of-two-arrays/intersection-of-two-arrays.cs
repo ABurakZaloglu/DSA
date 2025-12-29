1392501ab79a3bc24261bd94e8a77054bb58cb4e
Ahmet Burak Zaloğlu <94478382+ABurakZaloglu@users.noreply.github.com>
2025-12-30 01:41:00 +0300

    Time: 4 ms (62.08%) | Memory: 47.6 MB (45.30%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] Intersection(int[] nums1, int[] nums2) {
+        HashSet<int> res = new HashSet<int>();
+
+        for(int i = 0; i < nums1.Length; i++){
+            for(int j = 0; j < nums2.Length; j++){
+                if(nums1[i] == nums2[j]){
+                    res.Add(nums1[i]);
+                    break;
+                }
+            }
+        }
+
+        return res.ToArray();
+        
+
+    }
+}
